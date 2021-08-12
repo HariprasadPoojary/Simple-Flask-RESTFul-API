9fd445f996cf5f51e6119e24b0942ababafd5897
Hariprasad <haridx.poojary@gmail.com>
2021-08-12 15:10:53 +0530

    implement simple api that stores and returns video info

--- main.py
+++ main.py
@@ -1,23 +1,51 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
+video_out_args = reqparse.RequestParser()
+video_out_args.add_argument(
+    "name",
+    type=str,
+    required=True,
+    help="Required: Name of the video",
+)
+video_out_args.add_argument(
+    "likes",
+    type=int,
+    required=True,
+    help="Required: Likes on the video",
+)
+video_out_args.add_argument(
+    "views",
+    type=int,
+    required=True,
+    help="Required: Views of the video",
+)
 
-class HelloWorld(Resource):
+videos = {}
+
+
+class Base(Resource):
     def get(self):
         return {
-            "data": "Hello World!!",
-        }
-
-    def post(self):
-        return {
-            "data": "Posted",
+            "video": "/video",
         }
 
 
-api.add_resource(HelloWorld, "/")
+class Video(Resource):
+    def get(self, video_id):
+        return videos[video_id]
+
+    def put(self, video_id):
+        args = video_out_args.parse_args()
+        videos[video_id] = args
+        return {video_id: args}, 201
+
+
+api.add_resource(Base, "/")
+api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == "__main__":
     app.run(debug=True)
