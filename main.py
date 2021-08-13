36e71f39e4183500d6c190e2efa7c0894a0b2477
Hariprasad <haridx.poojary@gmail.com>
2021-08-13 20:21:51 +0530

    DRY code & PUT function

--- main.py
+++ main.py
@@ -27,6 +27,24 @@ video_out_args.add_argument(
 videos = {}
 
 
+def abort_video_exists(video_id):
+    # get video id
+    video = videos.get(video_id)
+    # send error message if video exists
+    if video:
+        abort(409, ErrorMessage="Video already exists!")
+
+
+def abort_video_doesnt_exists(video_id):
+    # get video id
+    video = videos.get(video_id)
+    # send error message if video doesn't exists
+    if not video:
+        abort(404, ErrorMessage="Video ID is not valid!")
+
+    return video
+
+
 class Base(Resource):
     def get(self):
         return {
@@ -36,24 +54,29 @@ class Base(Resource):
 
 class Video(Resource):
     def get(self, video_id):
-        video = videos.get(video_id)
-        if not video:
-            abort(404, ErrorMessage="Video ID is not valid!")
+        # validate video id
+        video = abort_video_doesnt_exists(video_id)
 
         return video, 200
 
     def put(self, video_id):
-        # get video id
-        video = videos.get(video_id)
-        # send error message if video exists
-        if video:
-            abort(409, ErrorMessage="Video already exists!")
+        # validate video id
+        abort_video_exists(video_id)
 
         # validate and add video info
         args = video_out_args.parse_args()
         videos[video_id] = args
+
         return {video_id: args}, 201
 
+    def delete(self, video_id):
+        # validate video id
+        video = abort_video_doesnt_exists(video_id)
+
+        if video:
+            del videos[video_id]
+
+        return 200
 
 api.add_resource(Base, "/")
 api.add_resource(Video, "/video/<int:video_id>")
